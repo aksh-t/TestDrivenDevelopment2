be6184b9d9a12895a559054e4741fe6a61b78b11
aksh-t <tanno.ao@gmail.com>
2020-03-30 01:21:45 +0900

    fib() の一般化、その4

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -5,7 +5,7 @@ class FibonacciTest:
     def fib(n):
         if n == 0:
             return 0
-        if n <= 2:
+        if n == 1:
             return 1
         return FibonacciTest.fib(n - 1) + FibonacciTest.fib(n - 2)
     
