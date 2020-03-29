3ff58ba3a604df2d3d85872bab910c1b4ac517c0
aksh-t <tanno.ao@gmail.com>
2020-03-30 01:26:49 +0900

    クラス名を変更＆クラスメソッド化

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,15 +1,15 @@
 from nose.tools import eq_
 
-class FibonacciTest:
-    @staticmethod
-    def fib(n):
+class Fibonacci:
+    @classmethod
+    def fib(cls, n):
         if n == 0:
             return 0
         if n == 1:
             return 1
-        return FibonacciTest.fib(n - 1) + FibonacciTest.fib(n - 2)
+        return cls.fib(n - 1) + cls.fib(n - 2)
     
 def test_fibonacci():
     cases = [[0, 0], [1, 1], [2, 1], [3, 2]]
     for case in cases:
-        eq_(case[1], FibonacciTest.fib(case[0]))
+        eq_(case[1], Fibonacci.fib(case[0]))
