5bc7e6dcb1f1f7d8bbb550d88a2e433b0b0ac8ea
aksh-t <tanno.ao@gmail.com>
2020-03-30 22:14:10 +0900

    三角形が成り立たない場合のテストを追加し、実装を修正 また、失敗した既存のテストも入力値を修正

--- triangle/test_triangle.py
+++ triangle/test_triangle.py
@@ -1,12 +1,16 @@
-from nose.tools import eq_
+from nose.tools import eq_, raises
 
-from triangle import judge
+from triangle import judge, NonTriangleException
 
 def test_equilateral():
     eq_(1, judge(1, 1, 1))
 
 def test_isosceles():
-    eq_(2, judge(1, 1, 2))
+    eq_(2, judge(2, 2, 1))
 
-def test_Scalene():
-    eq_(3, judge(1, 2, 3))
+def test_scalene():
+    eq_(3, judge(2, 3, 4))
+
+@raises(NonTriangleException)
+def test_non_triangle():
+    judge(1, 1, 100)
--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,6 +1,12 @@
 def judge(a, b, c):
+    if not(a < b + c and b < a + c and c < a + b):
+        raise NonTriangleException()
+
     if a == b == c:
         return 1
     elif a == b:
         return 2
     return 3
+
+class NonTriangleException(Exception):
+    pass
